8399b42adafbc2402bf30feb55539f90376c3c62
Jcast55 <80773868+Jcast55@users.noreply.github.com>
2023-11-15 16:20:32 -0600

    Update InformeController.java

--- src/main/java/com/web/app/Controller/InformeController.java
+++ src/main/java/com/web/app/Controller/InformeController.java
@@ -1,10 +1,6 @@
 package com.web.app.Controller;
 
 import java.util.List;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.MediaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.web.app.Model.Informe;
 import com.web.app.Service.InformeService;
@@ -50,72 +45,15 @@ public class InformeController {
     }
 
    
-@PutMapping(value = "/informes/{id}", consumes = {"application/json", "text/plain;charset=UTF-8"})
-public ResponseEntity<Informe> updateInforme(
-        @PathVariable Long id,
-        @RequestBody(required = false) String informeData,
-        @RequestHeader("Content-Type") String contentType) {
-
-    // Verifica el tipo de contenido de la solicitud
-    if (!contentType.equalsIgnoreCase("application/json") && !contentType.equalsIgnoreCase("text/plain;charset=UTF-8")) {
-        // Devuelve una respuesta de tipo de contenido no admitido si es diferente
-        return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-    }
-
-    // Si el cuerpo de la solicitud es nulo, puedes manejarlo según tus necesidades
-    if (informeData == null) {
-        // Devuelve una respuesta adecuada para el cuerpo de la solicitud nulo
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-
-    Informe updatedInforme;
-
-    // Procesa informeData según el tipo de contenido
-    if (contentType.equalsIgnoreCase("application/json")) {
-        // Procesa como JSON
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Informe informeObject = objectMapper.readValue(informeData, Informe.class);
-            updatedInforme = informeService.updateInforme(id, informeObject);
-        } catch (IOException e) {
-            e.printStackTrace(); // Manejar la excepción según tus necesidades
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    } else {
-        // Procesa como texto plano
-        String[] parts = informeData.split("\n");
-        
-        // Asegúrate de manejar el array 'parts' adecuadamente
-        // Crear un objeto Informe con los valores extraídos.
-
-        // Ejemplo básico (asegúrate de manejar los índices correctamente):
-        try {
-            Informe informeObject = new Informe();
-            informeObject.setHoraInicio(parseDate(parts[0]));
-            informeObject.setIdInforme1(parts[1]);
-            informeObject.setHorasTrabajadas(Integer.parseInt(parts[2]));
-            informeObject.setHoraFinalizado(parseDate(parts[3]));
-            informeObject.setIdContact(parts[4]);
-
-            updatedInforme = informeService.updateInforme(id, informeObject);
-        } catch (ParseException e) {
-            e.printStackTrace(); // Manejar la excepción según tus necesidades
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+ @PutMapping("/{id}")
+    public ResponseEntity<Informe> updateInforme(@PathVariable Long id, @RequestBody Informe informe) {
+        Informe updatedInforme = informeService.updateInforme(id, informe);
+        if (updatedInforme != null) {
+            return new ResponseEntity<>(updatedInforme, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-    if (updatedInforme != null) {
-        return new ResponseEntity<>(updatedInforme, HttpStatus.OK);
-    } else {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-}
-
-// Método para convertir String a Date
-private Date parseDate(String dateString) throws ParseException {
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-    return dateFormat.parse(dateString);
-}
 
 
     @DeleteMapping("/{id}")
@@ -137,3 +75,4 @@ private Date parseDate(String dateString) throws ParseException {
     }
 
 }
+
